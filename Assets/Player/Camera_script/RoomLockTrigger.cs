01a160fee7657a653a504a70b77d302f0091549e
Pupa Tha <pupa.tha12@gmail.com>
2025-12-29 00:34:07 +0700

    Camera Room

---
+++ Assets/Player/Camera_script/RoomLockTrigger.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+
+public class RoomLockTrigger : MonoBehaviour
+{
+    [SerializeField] private Transform roomAnchor;
+    [SerializeField] private float _roomOrthoSize = 10f;
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (!other.CompareTag("Player")) return;
+        CameraManager.instance.EnterRoomLock(roomAnchor);
+        CameraManager.instance.SetRoomOrthoSize(_roomOrthoSize);
+    }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        if (!other.CompareTag("Player")) return;
+        CameraManager.instance.ExitRoomLock();
+        CameraManager.instance.ResetRoomOrthoSize();
+    }
+}
