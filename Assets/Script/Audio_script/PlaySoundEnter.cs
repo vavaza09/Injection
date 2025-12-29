2b95782f86e6192a9283ad418d7618693f158c7e
Pupa Tha <pupa.tha12@gmail.com>
2025-12-30 00:52:20 +0700

    Re-infrastructure+Sound
    Update folder
    Add soundManager(no music yet)

---
+++ Assets/Script/Audio_script/PlaySoundEnter.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+
+public class PlaySoundEnter : StateMachineBehaviour
+{
+    [SerializeField] private SoundType sound;
+    [SerializeField, Range(0, 1)] private float volume = 1;
+
+    public override void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        SoundManager.PlayeSound(sound, volume);
+    }
+}
