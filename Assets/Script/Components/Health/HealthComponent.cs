fda274905ecd4ded066122ce56cc1ada2a4bcad0
Thanasorn Dusadeeroj <64414782+vavaza09@users.noreply.github.com>
2026-01-07 16:00:35 +0700

    Script(Implement): Implement class base Charactor and Dependency

---
+++ Assets/Script/Components/Health/HealthComponent.cs
@@ -0,0 +1,33 @@
+namespace Game.Components.Health
+{
+    public class HealthComponent
+    {
+        public float maxHealth { get; private set; }
+        public float currentHealth { get; private set; }
+
+        public void Initialize(float max)
+        {
+
+        }
+
+        public void TakeDamage(float amount)
+        {
+
+        }
+
+        public void Heal(float amount)
+        {
+
+        }
+
+        public float GetHealthPercentage()
+        {
+            return 0f;
+        }
+
+        public bool IsDead()
+        {
+            return false;
+        }
+    }
+}
