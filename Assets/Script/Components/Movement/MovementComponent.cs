d042b5f7b9bad2294e2c4ae41d0f9664a01a1211
Thanasorn Dusadeeroj <64414782+vavaza09@users.noreply.github.com>
2026-01-07 16:00:35 +0700

    Script(Implement): Implement class base Charactor and Dependency

---
+++ Assets/Script/Components/Movement/MovementComponent.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+
+namespace Game.Components.Movement
+{
+    public class MovementComponent
+    {
+        public float MoveSpeed { get; private set; }
+        
+        private Rigidbody2D rb;
+
+        public void Move(Vector2 direction)
+        { 
+
+        }
+
+        public void Stop()
+        {
+
+        }
+
+        public void SetSpeed(float speed)
+        {
+
+        }
+    }
+
+}
+
