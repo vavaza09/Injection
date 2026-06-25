51ecb538375558b146bcdd977981bb78a8bad97a
Pupa Tha <pupa.tha12@gmail.com>
2026-06-25 14:11:09 +0700

    Pause menu not finish

---
+++ Assets/Script/UI/PauseMenuController.cs
@@ -0,0 +1,102 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.SceneManagement;
+using Game.Characters.Player;
+
+public class PauseMenuController : MonoBehaviour
+{
+    [SerializeField] private GameObject menuRoot;
+    [SerializeField] private GameObject settingsPanel;
+    [SerializeField] private string titleSceneName = "TitleScene";
+
+    private bool _isPaused;
+    private bool _settingsOpen;
+    private InputAction _pauseAction;
+
+    private void Awake()
+    {
+        Debug.Log("[Pause] Awake");
+        _pauseAction = new InputAction("Pause", InputActionType.Button);
+        _pauseAction.AddBinding("<Keyboard>/escape");
+        _pauseAction.AddBinding("<Gamepad>/start");
+        _pauseAction.performed += OnPausePressed;
+        _pauseAction.Enable();
+        Debug.Log("[Pause] InputAction enabled, controls=" + _pauseAction.controls.Count);
+    }
+
+    private void Start()
+    {
+        Debug.Log("[Pause] Start, menuRoot=" + (menuRoot == null ? "NULL" : menuRoot.name));
+        menuRoot.SetActive(false);
+        settingsPanel.SetActive(false);
+        _isPaused = false;
+        _settingsOpen = false;
+    }
+
+    private void OnPausePressed(InputAction.CallbackContext ctx)
+    {
+        Debug.Log("[Pause] OnPausePressed isPaused=" + _isPaused + " settingsOpen=" + _settingsOpen);
+        if (_settingsOpen)
+            CloseSettings();
+        else if (_isPaused)
+            Resume();
+        else
+            Pause();
+    }
+
+    public void Pause()
+    {
+        Time.timeScale = 0f;
+        menuRoot.SetActive(true);
+        SetPlayerInput(false);
+        _isPaused = true;
+    }
+
+    public void Resume()
+    {
+        SoundManager.PlaySound(SoundType.UI_CLICK);
+        Time.timeScale = 1f;
+        menuRoot.SetActive(false);
+        settingsPanel.SetActive(false);
+        SetPlayerInput(true);
+        _isPaused = false;
+        _settingsOpen = false;
+    }
+
+    public void OpenSettings()
+    {
+        SoundManager.PlaySound(SoundType.UI_CLICK);
+        settingsPanel.SetActive(true);
+        _settingsOpen = true;
+    }
+
+    public void CloseSettings()
+    {
+        SoundManager.PlaySound(SoundType.UI_CLICK);
+        settingsPanel.SetActive(false);
+        _settingsOpen = false;
+    }
+
+    public void QuitToTitle()
+    {
+        SoundManager.PlaySound(SoundType.UI_CLICK);
+        Time.timeScale = 1f;
+        SceneManager.LoadScene(titleSceneName);
+    }
+
+    private void SetPlayerInput(bool on)
+    {
+        var playerGO = GameObject.FindWithTag("Player");
+        playerGO?.GetComponent<Player>()?.SetInputEnabled(on);
+    }
+
+    private void OnDestroy()
+    {
+        if (_pauseAction != null)
+        {
+            _pauseAction.performed -= OnPausePressed;
+            _pauseAction.Disable();
+            _pauseAction.Dispose();
+        }
+    }
+}
